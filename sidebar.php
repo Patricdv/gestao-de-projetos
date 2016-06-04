1ff2b91d833eb76849d3c59bcfb146fe654f7cef
RogerioSchmidt <rogelho.schmidt@gmail.com>
2016-06-03 21:47:48 -0300

    Adicionado Botao para cadastro de rota

--- sidebar.php
+++ sidebar.php
@@ -1,25 +1,21 @@
 <ul id="nav-mobile" class="side-nav fixed" style="width: 240px;">
-	<li class="logo">
-		<a id="logo-container" href="/" class="brand-logo"></a>
-	</li>
 	<li class="bold"><a href="/gestao-de-projetos/" class="waves-effect waves-teal">Home</a></li>
-	<li class="bold"><a href="getting-started.html" class="waves-effect waves-teal">Começando</a></li>
+
+
+	<?php 
+		
+		if (isset($_SESSION['user_id']) && !empty($_SESSION['user_id'])) { 	
+	?>
 	<li class="no-padding">
 	  <ul class="collapsible collapsible-accordion">
-	    <li class="bold"><a class="collapsible-header  waves-effect waves-teal">Components</a>
+	    <li class="bold"><a class="collapsible-header  waves-effect waves-teal">Cadastrar Rotas</a>
 	      <div class="collapsible-body" style="">
 	        <ul>
-	          <li><a href="preloader.html">Preloader</a></li>
-	        </ul>
-	      </div>
-	    </li>
-	    <li class="bold"><a class="collapsible-header  waves-effect waves-teal">JavaScript</a>
-	      <div class="collapsible-body" style="">
-	        <ul>
-	          <li><a href="collapsible.html">Collapsible</a></li>
+	          <li><a href="rota.php">Cadastro</a></li>
 	        </ul>
 	      </div>
 	    </li>
 	  </ul>
 	</li>
+	<?php } ?>
 </ul>
