de439950f79680b0ae05fb1376f7c167184c2f00
Patricdv <patric.venturini@hotmail.com>
2016-06-03 21:47:42 -0300

    colocado sessão

--- rota.php
+++ rota.php
@@ -1,3 +1,6 @@
+<?php 
+	session_start();
+?>	
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
